5121e230dbce4f36210db680d86984167a1525fb
Aristides Evandro <aristides.evan.01@gmail.com>
2021-07-02 18:12:56 -0300

    Aplicando pizzaJson no html

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-let c = (el)=> document.querySelector(el)
-let cs = (el)=> document.querySelector(el)
+let c = (el)=> document.querySelector(el);
+let cs = (el)=> document.querySelector(el);
 
 pizzaJson.map((item, index)=>{
-    let pizzaItem = c(".pizza-item").cloneNode(true)
+    var pizzaItem = c(".pizza-item").cloneNode(true);
 
-    c(".pizza-area").append(pizzaItem)
-})
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+    c(".pizza-area").append(pizzaItem);
+});

5be6fddcc3f4c094608e42a8c4aaf0c5454208b0
Aristides Evandro <aristides.evan.01@gmail.com>
2021-07-05 11:02:00 -0300

    adicionando os pesos das pizzas e fixando valores

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+let modalQt = 1;
+
 let c = (el)=> document.querySelector(el);
-let cs = (el)=> document.querySelector(el);
+let cs = (el)=> document.querySelectorAll(el);
 
 pizzaJson.map((item, index)=>{
     let pizzaItem = c(".pizza-item").cloneNode(true);
@@ -13,14 +15,27 @@ pizzaJson.map((item, index)=>{
         e.preventDefault();
 
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQt = 1;
+
 
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
         c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
 
+        c('.pizzaInfo--size.selected').classList.remove('selected');
 
-        console.log(pizzaJson[key]);
+        cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+
+            if(sizeIndex == 2){
+                size.classList.add('selected');
+            }
+
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+
+        });
+
+        c('.pizzaInfo--qt').innerHTML = modalQt;
 
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = 'flex';
